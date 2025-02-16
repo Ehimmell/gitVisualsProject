cbafaa66118d752d5d7a7c0f69dd948426288756
Ehimmell <himmellethan@gmail.com>
2025-02-16 01:03:38 -0600

    thumbnail features

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import '../TextCarousel/TextCarousel.js';
 import TextCarousel from "../TextCarousel/TextCarousel";
 import './Home.css';
+import techleaf from '../..//techleaf-edit.png';
+import techdocs from '../..//techpaper-edit.png';
 
 export default function Home({ handlePage }) {
     const [page, setPage] = useState('home');
@@ -12,6 +14,7 @@ export default function Home({ handlePage }) {
 
     return (
         <div className={'container-1'}>
+            <div className={'hook-container-1'}>
             <TextCarousel
                 texts={[
                     'Visualize a repo',
@@ -24,7 +27,18 @@ export default function Home({ handlePage }) {
                     style={{
                         paddingTop: '0px',
                         paddingBottom: '0px',
-                    }}>&#8594;</button>
+                    }}>Starting Guide &#8594;</button>
+            </div>
+            <div className={'feature-thumbnail-container'}>
+                <div>
+                    <img className={'search-thumbnail-container'}src = {techleaf} alt={'search-thumbnail'}/>
+                    <p>Search a Tree</p>
+                </div>
+                <div>
+                    <img className={'docs-thumbnail-container'} src = {techdocs} alt={'docs-thumbnail'}/>
+                    <p>View Documentation</p>
+                </div>
+            </div>
         </div>
     );
 }
