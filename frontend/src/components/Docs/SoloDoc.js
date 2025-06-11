419540c56ce73f9c15545e39845fc6ffce64d39c
Ehimmell <himmellethan@gmail.com>
2025-06-10 23:13:36 -0500

    add semi-finished doc procedural generation

--- frontend/src/components/Docs/SoloDoc.js
+++ frontend/src/components/Docs/SoloDoc.js
@@ -1,48 +1,51 @@
-import {React, useEffect, useState} from 'react';
-
+import { React } from 'react';
 import './Docs.css';
 
 export default function SoloDoc(props) {
-    const [clicked, setClicked] = useState(false);
     const json = props.json;
 
-    const openDoc =
-        (
-            <div className = {"open-doc-container"}>
-                {json?.Sections?.map((section, i) => (
-                    <div key={i}>
-                        <h1 className={"solo-doc-body"}>{section.Header}</h1>
-                        <p className={"solo-doc-body"}>{section.Body}</p>
-                        {section.Grid != null &&
-                            (
-                                <table className={"solo-doc-body"}>
-                                    <thead>
+    const openDoc = (
+        <div className={"open-doc-container"}>
+            <h2 className={"solo-doc-header"}>{json.Title}</h2>
+            <p className={"solo-doc-body"}>{json.Description}</p>
+            {json?.Sections?.map((section, i) => (
+                <div key={i}>
+                    <h3 className={"solo-doc-header"}>{section.Header}</h3>
+                    <p className={"solo-doc-body"}>{section.Body}</p>
+                    {section.Grid != null && section.GridColumns != null && (
+                        <div className={"solo-doc-body"}>
+                            <table className={"table"}>
+                                <thead>
                                     <tr>
-                                        <td>test row</td>
+                                        {section.GridColumns.map((column, j) => (
+                                            <th key={j}>{column}</th>
+                                        ))}
                                     </tr>
-                                    </thead>
-                                    <tbody>
-                                    {
-                                        section?.Grid?.map((item, j) => (
-                                            <tr key={j}>
-                                                <td>{item}</td>
-                                            </tr>
-                                        ))
-                                    }
-                                    </tbody>
-                                </table>
-                            )
-                        }
-                    </div>
-                ))}
-            </div>
-        )
+                                </thead>
+                                <tbody>
+                                    {section.Grid.map((row, j) => (
+                                        <tr key={j}>
+                                            {row.map((cell, k) => (
+                                                <td key={k}>{cell}</td>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
+                </div>
+            ))}
+        </div>
+    );
 
     const setOpenDoc = () => {
         props.setDoc(openDoc);
-    }
+    };
 
     return (
-        <p onClick = {setOpenDoc} className={"solo-doc-body"}>{json.Title}</p>
-    )
+        <p onClick={setOpenDoc} className={"solo-doc-body"}>
+            {json.Title}
+        </p>
+    );
 }
